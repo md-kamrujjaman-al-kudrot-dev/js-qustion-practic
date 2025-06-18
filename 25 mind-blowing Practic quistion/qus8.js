a88570a69f063c0184ed8482bf7395d2945b07c6
md-kudrot-592 <kk.mdkudrot@gmail.com>
2025-06-19 02:33:29 +0600

    qus8:  Write a JavaScript function that returns a passed string with letters in alphabetical order

---
+++ 25 mind-blowing Practic quistion/qus8.js
@@ -0,0 +1,11 @@
+// qus8:  Write a JavaScript function that returns a passed string with letters in alphabetical order
+
+function stringAlphaBeticalOrder(str){
+    return str.split("").sort().join("");
+}
+
+console.log(stringAlphaBeticalOrder("apple"))
+
+
+
+      
