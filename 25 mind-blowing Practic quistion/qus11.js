de603aead95a94ad63ccb5622ce67f00c7e575f2
md-kudrot-592 <kk.mdkudrot@gmail.com>
2025-06-26 01:05:04 +0600

    the solution of qus 11.

--- 25 mind-blowing Practic quistion/qus11.js
+++ 25 mind-blowing Practic quistion/qus11.js
@@ -1,1 +1,10 @@
 // loop an array and addd all members of it
+let arr = [1,2]
+
+let sum = 0 ;
+
+arr.forEach(element => {
+   sum = sum + element; 
+});
+
+console.log(sum)
---
+++ 25 mind-blowing Practic quistion/tempCodeRunnerFile.js
@@ -0,0 +1,9 @@
+3,4,5,6,7]
+
+// let sun = 0 ;
+
+// arr.forEach(element => {
+//    sum = sum + element; 
+// });
+
+// console.log(sum
