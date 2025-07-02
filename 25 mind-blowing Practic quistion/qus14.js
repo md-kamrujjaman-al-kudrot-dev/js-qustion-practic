2cbcea86d1518982c5b97c55ec9989ced7d26728
md-kudrot-592 <kk.mdkudrot@gmail.com>
2025-07-03 00:17:01 +0600

    write a JavaScript function which accepts an argument and returns the type

---
+++ 25 mind-blowing Practic quistion/qus14.js
@@ -0,0 +1,7 @@
+// write a JavaScript function which accepts an argument and returns the type 
+ function typeOfValue(elem){
+    return typeof elem;
+ }
+
+ console.log(typeOfValue([]))
+ console.log(typeOfValue(85))
