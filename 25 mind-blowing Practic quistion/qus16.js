f1084d2d673ef207749fdef64fe78c796007867a
md-kudrot-592 <kk.mdkudrot@gmail.com>
2025-07-06 23:54:53 +0600

    Add function to get the last 'n' elements of an array (Qus16)

---
+++ 25 mind-blowing Practic quistion/qus16.js
@@ -0,0 +1,12 @@
+
+function retrieve(arr,n = 1){
+    if (n <= arr.length){
+        for (let i = 0; i<n; i++){
+            console.log(arr[arr.length - 1 -i])
+        }
+    }else{
+        console.log(`Array does not contain ${n} elements.`)
+    }
+}
+
+retrieve([1,2,3,4,5,6], 5)
