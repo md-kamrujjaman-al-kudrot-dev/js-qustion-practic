a66d96e364530cbb23cc863e44d71db561ee5bca
md-kudrot-592 <kk.mdkudrot@gmail.com>
2025-07-10 01:54:57 +0600

    qus19: write a JavaScript program to compute the union of two arrays

---
+++ 25 mind-blowing Practic quistion/qus19.js
@@ -0,0 +1,10 @@
+// qus19: write a JavaScript program to compute the union of two arrays
+// Sample Data: 
+// console.log(union([1,2,3],[100,2,1,10]))
+// [1,2,3,10,100]
+
+function union(arr1,arr2){
+    return[...new Set(arr1.concat(arr2))]
+}
+
+console.log(union([1,2,3,3,3,3,3,3,4],[2,3,4,5,6,]))
